5c541d241aeb120ca469916692f25628090113f1
chrisbonifacio <christopher.bonifacio@gmail.com>
2019-10-17 15:59:41 -0400

    function now verifies jwt before allowing access to endpoint

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,13 +1,16 @@
-function restricted(req, res, next) {
-  const token = req.headers.token;
+const jwt = require("jsonwebtoken");
+const secrets = require("../../config/secrets");
 
-  if (!token) {
-    res
-      .status(400)
-      .json({ message: "Please send token in header to recieve users" });
-  } else {
-    next();
-  }
+function restricted(req, res, next) {
+  const token = req.headers.authorization;
+
+  jwt.verify(token, secrets.jwtSecret, err => {
+    if (err) {
+      res.status(400).json({ message: "JSON Token could not be verified" });
+    } else {
+      next();
+    }
+  });
 }
 
 module.exports = restricted;
