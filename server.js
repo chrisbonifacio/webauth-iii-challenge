528f293292568a6c3a5a9148b13ced1a4ecf6ce0
chrisbonifacio <christopher.bonifacio@gmail.com>
2019-10-17 13:30:44 -0400

    setup server

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require("express");
 const server = express();
 const helmet = require("helmet");
 
+const authRouter = require("./api/routes/authRouter");
+
 server.use(helmet());
-server.use(express());
+server.use(express.json());
+
+server.use("/api", authRouter);
 
 module.exports = server;
