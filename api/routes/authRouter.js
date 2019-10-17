50f45c08b66800f52864b94da82da172b5aed0fd
chrisbonifacio <christopher.bonifacio@gmail.com>
2019-10-17 13:31:57 -0400

    create auth endpoints

---
+++ api/routes/authRouter.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+
+const Users = require("../users/userModel");
+
+const login = require("../middleware/login");
+const restricted = require("../middleware/restricted");
+
+const generateToken = require("../tokens/generateToken");
+
+// GET all users
+router.get("/users", restricted, async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(400).json({ message: "Could not get users" });
+  }
+});
+
+// GET user by ID
+router.get("/users/:id", restricted, async (req, res) => {
+  try {
+    const user = await Users.find(req.params.id);
+    if (!user) {
+      res
+        .status(400)
+        .json({ message: "User with specified ID does not exist" });
+    } else {
+      res.status(200).json(user);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
+
+// POST to login
+router.post("/login", login, (req, res) => {
+  const user = req.user;
+  const token = generateToken(user);
+  res.status(200).json({ message: `Welcome ${user.username}!`, token });
+});
+
+module.exports = router;
